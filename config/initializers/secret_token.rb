7348bb227a1e0631ed672d6b8096da4b129fb6a7
Shane Andrade <mauidude2@gmail.com>
2012-01-30 21:31:54 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Glasspixel::Application.config.secret_token = '62f506a3b610adc6fe9d2f47dbaacf2f1829652baf85b701165860611e49098d51679e2b0ff194613f0a35dfcacc55a51179ce2b9a85e1b657b65686b3651709'
